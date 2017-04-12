80ae89d769a0863e5c79cffc3f0ccc90a5315c62
Arinze Ufoeze <arinze@techinceptions.com>
2017-04-12 13:22:03 -0400

    Added ConvertAll

---
+++ TECHIS.Core/ConversionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TECHIS.Core
+{
+    public static class ConversionExtensions
+    {
+        public static List<TResult> ConvertAll<TInput, TResult>(this IList<TInput> inputList, Func<TInput,TResult> converter )
+        {
+            List<TResult> outputList;
+            if (inputList == null)
+            {
+                outputList = null;
+            }
+            else
+            {
+                outputList = new List<TResult>(inputList.Count);
+                for (int i = 0; i < inputList.Count; i++)
+                {
+                    outputList[i] = converter(inputList[i]);
+                }
+            }
+
+            return outputList;
+        }
+    }
+}
