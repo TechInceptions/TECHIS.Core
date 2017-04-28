840272ff12a6bdb47f0052a5bd090556515ea532
Arinze Ufoeze <arinze@techinceptions.com>
2017-04-28 11:15:36 -0400

    Added IApplicationSettingsProvider and AppState

---
+++ TECHIS.Core/AppState.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TECHIS.Core
+{
+    public static class AppState
+    {
+        public static DateTime InitializedAt = DateTime.UtcNow;
+        public static Collections.ConcurrentPipe<KeyValuePair<string, string>> CurrentState { get; } = new Collections.ConcurrentPipe<KeyValuePair<string, string>>(50);
+        public static StringBuilder AsText()
+        {
+            var cs = CurrentState.ToArray();
+            StringBuilder sb = new StringBuilder();
+            
+            foreach (var kvp in cs)
+            {
+                sb.Append(kvp.Key).Append(" : ").AppendLine($"'{kvp.Value}'");
+            }
+
+            return sb;
+        }
+    }
+}
---
+++ TECHIS.Core/IApplicationSettingsProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TECHIS.Core
+{
+    public interface IApplicationSettingsProvider
+    {
+        Task<IEnumerable<KeyValuePair<string, string>>> GetAllAsync();
+        IEnumerable<KeyValuePair<string, string>> GetAll();
+        Task<string> GetAsync(string key);
+        string Get(string key);
+    }
+}
