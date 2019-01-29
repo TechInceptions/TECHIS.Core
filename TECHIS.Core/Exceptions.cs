753e3cb1f68034dd3a349ac0f3b57726061e1f12
Arinze Ufoeze <aufoeze@objectsharp.com>
2019-01-28 22:14:50 -0500

    Added functionality from the ExceptionsUtil (from the old Oxydata lib.) class to the exceptions class

--- TECHIS.Core/Exceptions.cs
+++ TECHIS.Core/Exceptions.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Diagnostics;
+using System.IO;
 
 namespace TECHIS.Core
 {
@@ -33,6 +34,57 @@ namespace TECHIS.Core
         public static void ThrowInvalidOps( params string[] messages )
         {
             ThrowInvalidOps(null, messages);
+        }
+        public static void Write(TextWriter tw, Exception xc)
+        {
+            var initialException = xc;
+            if (initialException != null)
+            {
+                int depth = 0;
+                while (xc != null)
+                {
+                    //Write message
+                    tw.WriteLine(xc.Message);
+
+                    //stack trace
+                    tw.WriteLine($"START: Stack Trace: {depth}");
+                    tw.WriteLine(xc.StackTrace);
+                    tw.WriteLine($"END: Stack Trace: {depth}");
+
+                    xc = xc.InnerException;
+                    depth++;
+                }
+
+                //stack trace
+                tw.WriteLine("START: Stack Trace: initialException");
+                tw.WriteLine(initialException.StackTrace);
+                tw.WriteLine("END: Stack Trace: initialException");
+            }
+
+        }
+        public static StringBuilder GetMessages(Exception xc)
+        {
+            StringBuilder sb = new StringBuilder(100);
+            using (System.IO.StringWriter sw = new System.IO.StringWriter(sb))
+            {
+                Write(sw, xc);
+            }
+            return sb;
+        }
+        public static StringBuilder GetMessages(Exception xc, params string[] additionalMessages)
+        {
+            StringBuilder sb = GetMessages(xc);
+            if (additionalMessages != null && additionalMessages.Length > 0)
+            {
+                sb.AppendLine();
+                sb.AppendLine("Additional messages: ");
+                foreach (string s in additionalMessages)
+                {
+                    sb.AppendLine(s);
+                }
+            }
+
+            return sb;
         }
     }
 }
