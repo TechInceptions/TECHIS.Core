0cf77440ad7f19be4d5ffddae5df7117e85dbbaf
Arinze Ufoeze <aufoeze@objectsharp.com>
2019-03-12 07:42:46 -0400

    Changed IApplicationSettings

---
+++ TECHIS.Core/ApplicationSettingsExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TECHIS.Core
+{
+    public static class ApplicationSettingsExtensions
+    {
+        public static TValue Get<TValue>(IApplicationSettings applicationSettings, string key, TValue value) where TValue : struct
+        {
+            var obj = applicationSettings.Get(key);
+
+            if (string.IsNullOrEmpty(obj))
+            {
+                return value;
+            }
+            else
+            {
+                return (TValue)ObjectReader.ParseObject(obj, typeof(TValue));
+            }
+        }
+        public static TValue Get<TValue>(IApplicationSettings applicationSettings, string key) where TValue : struct
+        {
+            return (TValue)ObjectReader.ParseObject(applicationSettings.Get(key), typeof(TValue));
+        }
+    }
+}
